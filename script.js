0de72d0684e1f4a00a22549933f3aee1429a7d83
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2021-09-12 16:48:36 -0700

    first files added

---
+++ script.js
@@ -0,0 +1,9 @@
+// small divices side-nav functions
+
+function openSideNav() {
+    document.getElementById("side-nav-sm").style.width  = "250px";
+}
+
+function closeSideNav() {
+    document.getElementById("side-nav-sm").style.width  = "0";
+}
